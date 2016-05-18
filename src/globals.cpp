1dd79bb0e9c7ebbb684cd3517e8f50a721c4bf39
Aria <blackstab1337@gmail.com>
2016-05-18 18:09:14 +0800

    Fixed formatting

--- src/globals.cpp
+++ src/globals.cpp
@@ -3,15 +3,10 @@
 sides::Side sides::get_opposite_side(const sides::Side& side)
 {
   switch(side) {
-    case Side::BOTTOM:
-      return Side::TOP;
-    case Side::TOP:
-      return Side::BOTTOM;
-    case Side::LEFT:
-      return Side::RIGHT;
-    case Side::RIGHT:
-      return Side::LEFT;
-    default:
-      return Side::NONE;
+    case Side::BOTTOM: return Side::TOP;
+    case Side::TOP: return Side::BOTTOM;
+    case Side::LEFT: return Side::RIGHT;
+    case Side::RIGHT: return Side::LEFT;
+    default: return Side::NONE;
   }
 }
